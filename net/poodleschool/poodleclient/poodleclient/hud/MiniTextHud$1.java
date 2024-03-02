aa927d84df80383e2bc5a7339b9fbc4bd0b3577c
Poodleschool <115908009+Poodleschool@users.noreply.github.com>
2024-03-01 18:35:28 -0700

    Delete net directory

--- net/poodleschool/poodleclient/poodleclient/hud/MiniTextHud$1.java
+++
@@ -1,7 +0,0 @@
-/*     */ package de.Jakob.navine.hud;
-
-
-/* Location:              /Users/iangoss/Downloads/navine-3.2.8.jar!/de/Jakob/navine/hud/MiniTextHud$1.class
- * Java compiler version: 17 (61.0)
- * JD-Core Version:       1.1.3
- */
--- net/poodleschool/poodleclient/poodleclient/modules/AttackEffects$1.java
+++
@@ -1,7 +0,0 @@
-/*    */ package net.poodleschool.poodleclient.poodleclient.modules;
-
-
-/* Location:              /Users/iangoss/Downloads/navine-3.2.8.jar!/de/Jakob/navine/modules/AttackEffects$1.class
- * Java compiler version: 17 (61.0)
- * JD-Core Version:       1.1.3
- */
